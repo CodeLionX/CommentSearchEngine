a3ba6f58f98624074ed06c68a9abd190179d9f73
CodeLionX <sebastian.schmidl@t-online.de>
2017-10-28 18:45:54 +0200

    added first version of bustom command cleanAll

--- setup.py
+++ setup.py
@@ -1,10 +1,50 @@
 #!/usr/bin/env python
 
 import os
+import distutils.cmd
+import distutils.log
+import subprocess
 from setuptools import setup
 
 with open(os.path.join('cse', 'VERSION')) as version_file:
     version = version_file.read().strip()
+
+class CleanAllCommand(distutils.cmd.Command):
+  """A custom command to clean the project directory."""
+
+  description = 'remove build artifacts'
+  user_options = [
+      # The format is (long option, short option, description).
+      ('includeDistributions=', 'd', 'removes distributions inside the `dist` folder as well'),
+      ('includeVagrant=', 'v', 'removes vagrant mashines and their state as well'),
+  ]
+
+  def initialize_options(self):
+    """Set default values for options."""
+    self.includeDistributions = False
+    self.includeVagrant = False
+
+  def finalize_options(self):
+    if self.includeDistributions:
+      assert self.includeDistributions in ['true', 'True'], (
+          '%s is not a valid option, use either true, True (shortcut: -d)' % self.includeDistributions)
+
+    if self.includeVagrant:
+      assert self.includeVagrant in ['true', 'True'], (
+          '%s is not a valid option, use either true, True (shortcut: -v)' % self.includeVagrant)
+
+  def run(self):
+    command = ['rm', '-rf', 'build/*', '*.egg-info']
+    if self.includeDistributions:
+        command.append('dist/*')
+    if self.includeVagrant:
+        command.append('.vagrant')
+
+    self.announce(
+        'Running command: %s' % str(command),
+        level=distutils.log.INFO)
+    subprocess.run(command, stdout=subprocess.PIPE, shell=True, cwd=os.getcwd())
+    #subprocess.check_call(command)
 
 setup(
     name='CommentSearchEngine',
@@ -14,6 +54,11 @@ setup(
     author_email='mail@benedikt1992.de, sebastian.schmidl@t-online.de',
     url='https://github.com/CodeLionX/CommentSearchEngine',
     license='MIT',
+
+    # own commands
+    cmdclass={
+        'cleanAll': CleanAllCommand,
+    },
 
     # dependencies
     install_requires=[
