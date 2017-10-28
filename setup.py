a0d339d7e6db20cc84e0366b9a7b221c909f796f
CodeLionX <sebastian.schmidl@t-online.de>
2017-10-28 19:03:51 +0200

    more fluent argument handling, but does not work in vagrant mashine

--- setup.py
+++ setup.py
@@ -6,45 +6,45 @@ import distutils.log
 import subprocess
 from setuptools import setup
 
+
 with open(os.path.join('cse', 'VERSION')) as version_file:
     version = version_file.read().strip()
 
+
 class CleanAllCommand(distutils.cmd.Command):
-  """A custom command to clean the project directory."""
+    """A custom command to clean the project directory."""
 
-  description = 'remove build artifacts'
-  user_options = [
-      # The format is (long option, short option, description).
-      ('includeDistributions=', 'd', 'removes distributions inside the `dist` folder as well'),
-      ('includeVagrant=', 'v', 'removes vagrant mashines and their state as well'),
-  ]
+    description = 'remove build artifacts from build, *dist, etc'
+    user_options = [
+        # The format is (long option, short option, description).
+        ('includeDistributions', 'd', 'removes distributions inside the `dist` folder as well'),
+        ('includeVagrant', 'v', 'removes vagrant mashines and their state as well'),
+    ]
 
-  def initialize_options(self):
-    """Set default values for options."""
-    self.includeDistributions = False
-    self.includeVagrant = False
+    def initialize_options(self):
+        """Set default values for options."""
+        self.includeDistributions = None
+        self.includeVagrant = None
 
-  def finalize_options(self):
-    if self.includeDistributions:
-      assert self.includeDistributions in ['true', 'True'], (
-          '%s is not a valid option, use either true, True (shortcut: -d)' % self.includeDistributions)
 
-    if self.includeVagrant:
-      assert self.includeVagrant in ['true', 'True'], (
-          '%s is not a valid option, use either true, True (shortcut: -v)' % self.includeVagrant)
+    def finalize_options(self):
+        """ check provided args """
 
-  def run(self):
-    command = ['rm', '-rf', 'build/*', '*.egg-info']
-    if self.includeDistributions:
-        command.append('dist/*')
-    if self.includeVagrant:
-        command.append('.vagrant')
 
-    self.announce(
-        'Running command: %s' % str(command),
-        level=distutils.log.INFO)
-    subprocess.run(command, stdout=subprocess.PIPE, shell=True, cwd=os.getcwd())
-    #subprocess.check_call(command)
+    def run(self):
+        command = ['rm', '-rf', 'build/*', '*.egg-info', '*.dist-info']
+        if self.includeDistributions:
+            command.append('dist/*')
+        if self.includeVagrant:
+            command.append('.vagrant')
+
+        self.announce(
+            'Running command: %s' % str(command),
+            level=distutils.log.INFO)
+        subprocess.run(command, stdout=subprocess.PIPE, shell=True, cwd=os.getcwd())
+
+# end class CleanAllCommand
+
 
 setup(
     name='CommentSearchEngine',
