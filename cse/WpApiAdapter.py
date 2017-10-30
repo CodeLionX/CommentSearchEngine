7daa2e6dfe79126d2b86ee55037305bcc4d33465
Sebastian Schmidl <sebastian.schmidl@sap.com>
2017-10-30 17:34:52 +0100

    added recursion for nested replies

--- cse/WpApiAdapter.py
+++ cse/WpApiAdapter.py
@@ -60,30 +60,35 @@ class WpApiAdapter:
         payload = self.buildInitialRequstPayload(url)
 
         response = requests.request("POST", 
-            self.apiEndpoint, 
-            data = self.toJsonString(payload), 
+            self.apiEndpoint,
+            data = self.toJsonString(payload),
             headers = {'content-type': 'application/json'}
         )
 
         data = self.fromJsonString(response.text)
         assetId = data['data']['asset']['id']
-        assetUrl = data['data']['asset']['url']
-        commentCount = data['data']['asset']['commentCount']
-        totalCommentCount = data['data']['asset']['totalCommentCount']
-        commentsHasNextPage = data['data']['asset']['comments']['hasNextPage']
-        commentsCursor = data['data']['asset']['comments']['endCursor']
-        comments = data['data']['asset']['comments']['nodes']
+        #assetUrl = data['data']['asset']['url']
+        #commentCount = data['data']['asset']['commentCount']
+        #totalCommentCount = data['data']['asset']['totalCommentCount']
+        commentsNode = data['data']['asset']['comments']
+        
+        return self.processComments(commentsNode, assetId)
 
-        # check for missing replies
+
+    def processComments(self, commentsNode, assetId):
+        commentsHasNextPage = commentsNode['hasNextPage']
+        commentsCursor = commentsNode['endCursor']
+        comments = commentsNode['nodes']
+
+        # check for replies
         for com in comments:
             parentId = com['id']
             repliesHasNextPage = com['replies']['hasNextPage']
-            repliesCursor = com['replies']['hasNextPage']
+            repliesCursor = com['replies']['endCursor']
             replies = com['replies']['nodes']
 
             if(repliesHasNextPage):
                 com['replies']['nodes'] = replies + self.loadMoreReplies(assetId, repliesCursor, parentId)
-
 
         # check for another page
         if(commentsHasNextPage):
@@ -91,58 +96,54 @@ class WpApiAdapter:
 
         return comments
 
+
     def loadMoreComments(self, assetId, cursor):
         payload = self.buildMoreRequestPayload(assetId, cursor=cursor)
 
         response = requests.request("POST", 
-            self.apiEndpoint, 
-            data = self.toJsonString(payload), 
+            self.apiEndpoint,
+            data = self.toJsonString(payload),
             headers = {'content-type': 'application/json'}
         )
 
         data = self.fromJsonString(response.text)
-        hasNextPage = data['data']['comments']['hasNextPage']
-        nextCursor = data['data']['comments']['endCursor']
-        comments = data['data']['comments']['nodes']
+        commentsNode = data['data']['comments']
 
-        if(hasNextPage):
-            comments = comments + self.loadMoreComments(assetId, nextCursor)
+        return self.processComments(commentsNode, assetId)
 
-        return comments
 
     def loadMoreReplies(self, assetId, cursor, parentId):
         payload = self.buildMoreRequestPayload(assetId, cursor=cursor, parentId=parentId)
 
         response = requests.request("POST", 
-            self.apiEndpoint, 
-            data = self.toJsonString(payload), 
+            self.apiEndpoint,
+            data = self.toJsonString(payload),
             headers = {'content-type': 'application/json'}
         )
 
         data = self.fromJsonString(response.text)
-        hasNextPage = data['data']['comments']['hasNextPage']
-        nextCursor = data['data']['comments']['endCursor']
-        comments = data['data']['comments']['nodes']
+        commentsNode = data['data']['comments']
 
-        if(hasNextPage):
-            replies = replies + self.loadMoreReplies(assetId, nextCursor, parentId)
+        return self.processComments(commentsNode, assetId)
 
-        return replies
 
     def countAllComments(self, comments):
-        count = 0
-        for comment in comments:
-            for reply in comment['replies']['nodes']:
-                count = count + 1
-            count = count + 1
-        return count
+        def countInList(comments, count):
+            for comment in comments:
+                count = countInNode(comment, count)
+            return count
+
+        def countInNode(comment, count):
+            if('replies' in comment and 'nodes' in comment['replies']):
+                return countInList(comment['replies']['nodes'], count) + 1
+            else:
+                return count + 1
+
+        return countInList(comments, 0)
 
 
 # just for testing
 if __name__ == "__main__":
     api = WpApiAdapter()
     comments = api.loadComments(url="https://www.washingtonpost.com/politics/courts_law/supreme-court-to-consider-major-digital-privacy-case-on-microsoft-email-storage/2017/10/16/b1e74936-b278-11e7-be94-fabb0f1e9ffb_story.html")
-    #print(comments.amount)
-    print(len(comments))
-    print("\n" + str(api.countAllComments(comments)))
-    #print(api.loadInitialQuery())
+    print(str(api.countAllComments(comments)))
