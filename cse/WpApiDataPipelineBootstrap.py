e49ebe8750384041b6c451ae8828d8458e576fc3
CodeLionX <sebastian.schmidl@t-online.de>
2017-11-03 01:02:37 +0100

    refactoring and support for dynamic listener exchange

--- cse/WpApiDataPipelineBootstrap.py
+++ cse/WpApiDataPipelineBootstrap.py
@@ -1,23 +1,27 @@
+from threading import Lock
 from cse.WpApiAdapter import WpApiAdapter
 from cse.WpApiParser import WpApiParser
 from cse.CSVWriter import CSVWriter
 from cse.pipeline import (Pipeline, SyncedHandlerContextFactory, Handler)
 from cse.pipeline.wpHandler import (WpApiAdapterHandler, WpApiParserHandler, ListenerHandler)
 
-class WpApiDataPipelineBootstrap:
+class WpApiDataPipelineBootstrap(Handler):
 
     __wasPipeBuild = False
     __wpApiAdapter = None
     __pipeline = None
 
     __listeners = []
+    __listenersLock = None
 
     __countHandler = None
 
     def __init__(self):
+        super().__init__("PipelineBootstrap for data listeners")
         self.__wpApiAdapter = WpApiAdapter()
         self.__wasPipeBuild = False
         self.__countHandler = CountHandler("Counter")
+        self.__listenersLock = Lock()
 
 
     def setupPipeline(self, asynchronous=False):
@@ -35,28 +39,34 @@ class WpApiDataPipelineBootstrap:
         self.__pipeline.addLast(WpApiParserHandler("WashingtonPostParser", WpApiParser())) # recursive datastructures -> flat datastructures
         self.__pipeline.addLast(self.__countHandler)
         self.__pipeline.addLast(DebugHandler("DebugHandler"))
-        self.__pipeline.addLast(ListenerHandler(self.__listeners)) # _ -> listeners
+        self.__pipeline.addLast(self) # _ -> listeners
 
         self.__wasPipeBuild = True
 
 
-    def process(self, url):
+    # Overrides Handler.process()
+    def process(self, ctx, data):
+        with self.__listenersLock:
+            for callback in self.__listeners:
+                callback(data)
+
+
+    def crawl(self, url):
         if not self.__wasPipeBuild:
             raise Exception("Pipeline uninitialized! First init pipeline with setupPipeline()")
         self.__wpApiAdapter.loadComments(url)
-        print(self.__countHandler.getCount())
+        print("Processed comments: " + str(self.__countHandler.getCount()))
 
 
     def registerDataListener(self, listener):
-        if self.__wasPipeBuild:
-            raise Exception("Pipeline already running! Can't add listeners at runtime")
-        self.__listeners.append(listener)
+        print("adding " + str(listener))
+        self.__listeners.append(listener) # append() is atomic
 
 
     def unregisterDataListener(self, listener):
-        if self.__wasPipeBuild:
-            raise Exception("Pipeline already running! Can't remove listeners at runtime")
-        self.__listeners.remove(listener)
+        with self.__listenersLock:
+            print("removing " + str(listener))
+            self.__listeners.remove(listener)
 
 
 
@@ -84,7 +94,17 @@ if __name__ == "__main__":
     writer.open()
     writer.printHeader()
     bs = WpApiDataPipelineBootstrap()
-    bs.registerDataListener(writer.printData)
+    printListener = writer.printData
+    bs.registerDataListener(printListener)
     bs.setupPipeline()
-    bs.process(url='https://www.washingtonpost.com/politics/courts_law/supreme-court-to-consider-major-digital-privacy-case-on-microsoft-email-storage/2017/10/16/b1e74936-b278-11e7-be94-fabb0f1e9ffb_story.html')
+    bs.crawl(url='https://www.washingtonpost.com/politics/courts_law/supreme-court-to-consider-major-digital-privacy-case-on-microsoft-email-storage/2017/10/16/b1e74936-b278-11e7-be94-fabb0f1e9ffb_story.html')
+    bs.unregisterDataListener(printListener)
+    writer.close()
+
+    ## try changing data listener
+    writer = CSVWriter("data/file2")
+    writer.open()
+    writer.printHeader()
+    bs.registerDataListener(writer.printData)
+    bs.crawl(url='https://www.washingtonpost.com/news/wonk/wp/2017/11/02/winners-and-losers-in-the-gop-tax-plan/?hpid=hp_hp-top-table-main_tax-winnerslosers-230pm%3Ahomepage%2Fstory&utm_term=.d2381570c5bc')
     writer.close()
