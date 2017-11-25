7b7f7e59802406f62c80ca4bec8e828c92ba1196
CodeLionX <sebastian.schmidl@t-online.de>
2017-11-25 18:26:01 +0100

    boolean search version without nested prefix searches operable

--- cse/SearchEngine.py
+++ cse/SearchEngine.py
@@ -1,5 +1,6 @@
 import os
 import re
+import functools
 
 from cse.lang import PreprocessorBuilder
 from cse.lang.PreprocessorStep import PreprocessorStep
@@ -34,7 +35,7 @@ class SearchEngine():
             .addCustomStepToEnd(CustomPpStep())
             .build()
         )
-        self.__boolQueryPattern = re.compile('^([\w\d*]+[^\S\x0a\x0d]*(NOT|OR|AND)[^\S\x0a\x0d]*[\w\d*]+)$', re.M)
+        self.__boolQueryPattern = re.compile('^([\w\d*]+[^\S\x0a\x0d]*(NOT|OR|AND)[^\S\x0a\x0d]*[\w\d*]+)+$', re.M)
         self.__prefixQueryPattern = re.compile('^[^\S\x0a\x0d]*([\w\d]*\*)[^\S\x0a\x0d]*$', re.I | re.M)
         self.__phraseQueryPattern = re.compile('^[^\S\x0a\x0d]*(\'[\w\d]+([^\S\x0a\x0d]*[\w\d]*)*\')[^\S\x0a\x0d]*$', re.I | re.M)
 
@@ -48,20 +49,71 @@ class SearchEngine():
         indexer.index()
 
 
+    def __cidSetCombiner(self, op):
+        def notFunc(x, y):
+            return x - y
+        def andFunc(x, y):
+            return x & y
+        def orFunc(x, y):
+            return x | y
+
+        switcher = {
+            Operator.NOT: notFunc,
+            Operator.AND: andFunc,
+            Operator.OR:  orFunc
+        }
+        return switcher.get(op)
+
+
     def __booleanSearch(self, query):
         # TODO: implement bool query parser
+        ii = self.loadIndex("data")
         # operator precedence: STAR > NOT > AND > OR
-        p = BooleanQueryParser(query)
-        print(p)
+        p = BooleanQueryParser(query).get()
+        cidSets = []
 
-        if Operator.STAR in p.get():
+        
+
+        if Operator.STAR in p:
             print("STAR operator found: prefixSearch needed")
-        return []
+            cids = []
+
+        else:
+            # filter out operators
+            # note: we only support one opperator kind per query at the moment!
+            op = None
+            if Operator.NOT in p:   op = Operator.NOT
+            elif Operator.OR in p:  op = Operator.OR
+            elif Operator.AND in p: op = Operator.AND
+            else:
+                print("No or wrong operator in query!!")
+                return []
+            terms = [term for term in p if term not in Operator]
+
+            # load document set per term
+            for term in terms:
+                pTerm = self.__prep.processText(term)
+                if not pTerm or len(pTerm) > 1:
+                    print(
+                        self.__class__.__name__ + ":", "term", term,
+                        "is invalid! Please use only one word for boolean queries."
+                    )
+                    return []
+
+                cidTuples = ii.retrieve(pTerm[0][0])
+                if cidTuples:
+                    cidSets.append(set( (cid for cid, _ in cidTuples) ))
+
+            firstCids = cidSets[0]
+            cidSets.remove(firstCids)
+            cids = functools.reduce(self.__cidSetCombiner(op), cidSets, firstCids)  
+
+        return self.__loadDocumentTextForCids(cids)
 
 
     def __prefixSearch(self, query):
         # TODO: implement prefix query parser
-        return []
+        return self.__loadDocumentTextForCids([])
 
 
     def __phraseSearch(self, query):
@@ -100,6 +152,9 @@ class SearchEngine():
         results = []
         commentPointers = set()
 
+        if not cids:
+            return results
+
         # get document pointers
         with DocumentMap(os.path.join("data", "documentMap.index")).open() as documentMap:
             for cid in cids:
@@ -115,7 +170,6 @@ class SearchEngine():
                     results.append(rowData["comment_text"])
 
         return results
-
 
 
     def __documentsWithConsecutiveTerms(self, firstTermTuples, secondTermTuples):
@@ -149,7 +203,7 @@ class SearchEngine():
             if re.search('NOT|AND|OR|[*]', query):
                 print("*** ERROR ***")
                 #raise ValueError("Query not supported. Please use only one of the following Operators: '*', 'NOT', 'AND', 'OR'")
-                return ["*** ERROR ***", "Query not supported. Please use only one of the following Operators or none: '*', 'NOT', 'AND', 'OR'"]
+                return ["*** ERROR ***", "Query not supported. Please use only one of the following binary Operators or none: '*', 'NOT', 'AND', 'OR'"]
 
             else:
                 print("\n\n##### Keyword Search")
@@ -171,6 +225,8 @@ class SearchEngine():
         #print(prettyPrint(self.search("hate")[:5]))
         #print(prettyPrint(self.search("prefix*")[:5]))
         #print(prettyPrint(self.search("party AND chancellor NOT europe")))
+        print(prettyPrint(self.search("NOT hate")[:5]))
+        print(prettyPrint(self.search("Trump AND hate")[:5]))
 
         #print(prettyPrint(self.search("party AND chancellor")[:5]))
         #print(prettyPrint(self.search("party NOT politics")[:5]))
@@ -178,9 +234,9 @@ class SearchEngine():
         #print(prettyPrint(self.search("euro* NOT europe")[:5]))
         #print(prettyPrint(self.search("publi* OR moderation")[:5]))
         #print(prettyPrint(self.search("'the european union'")[:5]))
-        print(prettyPrint(self.search("'christmas market'")))
-        print(prettyPrint(self.search("'Republican legislation'")))
-        print(prettyPrint(self.search("'truck confederate flag'")))
+        #print(prettyPrint(self.search("'christmas market'")))
+        #print(prettyPrint(self.search("'Republican legislation'")))
+        #print(prettyPrint(self.search("'truck confederate flag'")))
 
 
 
