dbffff3923292d9ac7b7b0382e3b8518bc001071
CodeLionX <sebastian.schmidl@t-online.de>
2017-11-30 21:18:50 +0100

    start building a html tag filter

---
+++ cse/lang/HtmlStopwordFilter.py
@@ -0,0 +1,24 @@
+import re
+
+def __clean_html(html):
+    """
+    Copied from NLTK package.
+    Remove HTML markup from the given string.
+
+    :param html: the HTML string to be cleaned
+    :type html: str
+    :rtype: str
+    """
+
+    # First we remove inline JavaScript/CSS:
+    cleaned = re.sub(r"(?is)<(script|style).*?>.*?(</\1>)", "", html.strip())
+    # Then we remove html comments. This has to be done before removing regular
+    # tags since comments can contain '>' characters.
+    cleaned = re.sub(r"(?s)<!--(.*?)-->[\n]?", "", cleaned)
+    # Next we can remove the remaining tags:
+    cleaned = re.sub(r"(?s)<.*?>", " ", cleaned)
+    # Finally, we deal with whitespace
+    cleaned = re.sub(r"&nbsp;", " ", cleaned)
+    cleaned = re.sub(r"  ", " ", cleaned)
+    cleaned = re.sub(r"  ", " ", cleaned)
+    return cleaned.strip()
